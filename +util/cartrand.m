50c098dfe5e52bfeec9590c2baee196c2fe0ee3f
Mehrdad Baghery <baghery.mehrdad@gmail.com>
2015-09-10 19:37:23 +0200

    working order, changed classgp to only covSEard

---
+++ +util/cartrand.m
@@ -0,0 +1,8 @@
+function components = cartrand(n)
+%CARTRAND Generate random numbers on an n dimensional sphere
+%   The output is the components of a vector with its tip on an
+%   n-dimensional sphere; it is a column vector.
+
+  
+
+end
