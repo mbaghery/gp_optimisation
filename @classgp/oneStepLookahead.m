f3bf458117511d0fd61f81a1a095fda11c31d587
Mehrdad Baghery <baghery.mehrdad@gmail.com>
2016-01-25 19:06:48 +0100

    "Everything works smoothly now. I am using GP to find the best laser shape from qprop."

--- @classgp/oneStepLookahead.m
+++ @classgp/oneStepLookahead.m
@@ -10,21 +10,29 @@ function [f, df] = oneStepLookahead(this, xs)
   end
 
   [m, k, dm, dk] = this.predictMAP(xs);
-%   [m, k, dm, dk] = this.predictBBQ(xs);
+%   [m, k, dm, dk] = this.predictAffine(xs);
 
   eta = min(this.Y);
 
   % if the training set is empty
   if (isempty(eta)); eta = min(m); end
-  
-  
+
+
 %   f = 1/2 * (m + eta - sqrt(2/pi)*s.*exp(-(m-eta).^2./(2*s.^2)) ...
 %     + (eta-m).*erf((m-eta)./(sqrt(2)*s)));
 
 
-  f = 1/2 * (eta + m) ...
-    + 1/2 * (eta - m) .* erf((m-eta) ./ sqrt(2*k)) ...
-    - sqrt(k / (2*pi)) .* exp(-(m-eta).^2 ./ (2*k));
+%   f = 1/2 * (eta + m) ...
+%     + 1/2 * (eta - m) .* erf((m-eta) ./ sqrt(2*k)) ...
+%     - sqrt(k / (2*pi)) .* exp(-(m-eta).^2 ./ (2*k));
+
+
+  normalCDF = 0.5 * erfc((m-eta) ./ sqrt(2*k));
+  normalPDF = exp(-(m-eta).^2 ./ (2*k)) ./ sqrt(2*pi*k);
+
+  f = eta ...
+    + normalCDF .* (m-eta) ...
+    - normalPDF .* k;
 
 
   % calculate the derivative only if it is needed
@@ -35,9 +43,12 @@ function [f, df] = oneStepLookahead(this, xs)
 %   df =  -1/2 * bsxfun(@times, dm, erf((m-eta)./(sqrt(2)*s))-1) ...
 %    - bsxfun(@times, ds, exp(-(eta - m).^2./(2*s.^2))/sqrt(2*pi));
 
-  df = -1/2 * bsxfun(@times, dm, erf((m-eta) ./ sqrt(2*k)) - 1) ...
-       -1/2 * bsxfun(@times, dk, exp(-(m-eta).^2 ./ (2*k)) ./ sqrt(2*pi*k));
-     
+%   df = -1/2 * bsxfun(@times, dm, erf((m-eta) ./ sqrt(2*k)) - 1) ...
+%        -1/2 * bsxfun(@times, dk, exp(-(m-eta).^2 ./ (2*k)) ./ sqrt(2*pi*k));
+
+  df = bsxfun(@times, normalCDF, dm) ...
+     - bsxfun(@times, normalPDF, 0.5*dk);
+
   % sometimes k could be zero, which leads to NaN. Therefore, let's kill
   % them bitches
   df(isnan(df))=0;
